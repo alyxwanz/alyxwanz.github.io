4bb7fbfadc26b2dc15243bc23b8f2a64dcdb9086
Alex Pochatovskiy <alexpochatovskiy@MacBook-Pro-Alex.local>
2020-10-21 20:40:20 +0300

    added logic for 0 value in powBtn

--- HW8/script/pow.js
+++ HW8/script/pow.js
@@ -14,12 +14,17 @@ function pow(a,b) {
 function powBtn () {
     let a = parseInt(prompt('please, write your number'));
     let b = parseInt(prompt('please, write your number'));
+    let res;
 
-    if (b < 1) {
+    if (b < 0) {
         alert('invalid data, plese write natural number');
+    } else if (b == 0) {
+        res = 1;
     } else {
-        alert(pow(a,b));
+        res = pow(a, b);
     }
+
+    alert(res);
 }
 
 
