485406c190a75fdc0f10226ce4f42bc4eab3184e
Alex Pochatovskiy <alexpochatovskiy@MacBook-Pro-Alex.local>
2020-11-01 21:02:02 +0200

    add while

--- hw9/script/script.js
+++ hw9/script/script.js
@@ -1,14 +1,35 @@
+// function numbersGame() {
+//     let userNumber = prompt('enter your number');
+//     let puzzleNumber = 5;
+
+//     if (userNumber === '' || isNaN(parseInt(userNumber))) {
+//         alert('please, enter your number');
+//     } else if (parseInt(userNumber) === puzzleNumber) {
+//         alert('you win!');
+//     } else {
+//         alert(parseInt(userNumber) < puzzleNumber ? 'more': 'less');
+//     }
+// }
+
+// numbersGame()
 
 function numbersGame() {
     let userNumber = prompt('enter your number');
     let puzzleNumber = 5;
 
-    if (userNumber === '' || isNaN(parseInt(userNumber))) {
-        alert('please, enter your number');
-    } else if (parseInt(userNumber) === puzzleNumber) {
-        alert('you win!');
-    } else {
-        alert(parseInt(userNumber) < puzzleNumber ? 'more': 'less');
+    while (parseInt(userNumber) !== puzzleNumber) {
+        if (parseInt(userNumber) < puzzleNumber) {
+            alert('more');
+            userNumber = prompt('enter your number');
+        } else if (parseInt(userNumber) > puzzleNumber){
+            alert('less');
+            userNumber = prompt('enter your number');
+        } else if (userNumber === '' || isNaN(parseInt(userNumber))){
+            alert('please, write your number');
+            userNumber = prompt('enter your number');
+        } else {
+            alert('you win!');
+        }
     }
 }
 
