5462ae9f3a3cef46507b4921ea85299b94726277
Alex Pochatovskiy <alexpochatovskiy@MacBook-Pro-Alex.local>
2020-11-01 23:33:24 +0200

    chng some mistakes

--- hw9/script/script.js
+++ hw9/script/script.js
@@ -1,36 +1,23 @@
-// function numbersGame() {
-//     let userNumber = prompt('enter your number');
-//     let puzzleNumber = 5;
-
-//     if (userNumber === '' || isNaN(parseInt(userNumber))) {
-//         alert('please, enter your number');
-//     } else if (parseInt(userNumber) === puzzleNumber) {
-//         alert('you win!');
-//     } else {
-//         alert(parseInt(userNumber) < puzzleNumber ? 'more': 'less');
-//     }
-// }
-
-// numbersGame()
-
 function numbersGame() {
-    let userNumber = prompt('enter your number');
+    let userNumber = getUserNumber();
     let puzzleNumber = 5;
-
-    while (parseInt(userNumber) !== puzzleNumber) {
-        if (parseInt(userNumber) < puzzleNumber) {
-            alert('more');
-            userNumber = prompt('enter your number');
-        } else if (parseInt(userNumber) > puzzleNumber){
-            alert('less');
-            userNumber = prompt('enter your number');
-        } else if (userNumber === '' || isNaN(parseInt(userNumber))){
-            alert('please, write your number');
-            userNumber = prompt('enter your number');
+    while (userNumber !== puzzleNumber) {
+        if (userNumber === '' || isNaN(userNumber)) {
+            alert('please, enter your number');
         } else {
-            alert('you win!');
+            alert(userNumber < puzzleNumber ? 'more': 'less');
         }
+        userNumber = getUserNumber();
+    }
+    if (userNumber === puzzleNumber) {
+        alert('you win!');
+        return;
     }
 }
 
 numbersGame()
+
+
+function getUserNumber() {
+    return parseInt(prompt('enter your number'));
+}
