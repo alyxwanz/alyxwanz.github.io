e0b7784f16947aea9a09f2476a9b4015bb75de38
alyxwanz <72573955+alyxwanz@users.noreply.github.com>
2020-11-08 15:38:39 +0200

    Add files via upload

---
+++ hw14/script/script.js
@@ -0,0 +1,12 @@
+let fruit = [
+    'Apple',
+    'Banana',
+    'Pineapple'
+];
+
+fruit.push('Orange', 'Grapes');
+fruit[1] = 'Peer';
+console.log(fruit.pop());
+fruit.unshift('Watermelon');
+
+console.log(fruit);
